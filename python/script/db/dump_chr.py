ee4056f9843b9a1f8520ccaf5418a2955833214c
Graham McVicker <gmcvicker@uchicago.edu>
2012-05-11 15:18:51 -0500

    dump all chromosomes, not just autos and X

--- python/script/db/dump_chr.py
+++ python/script/db/dump_chr.py
@@ -7,7 +7,13 @@ import genome.db
 def main():
     gdb = genome.db.GenomeDB()
 
-    for chrom in gdb.get_chromosomes():
+    for chrom in gdb.get_chromosomes(get_rand=True,
+				     get_auto=True,
+				     get_sex=True,
+				     get_x=True,
+				     get_y=True,
+				     get_hap=True,
+				     get_mito=True):
         print "%s\t%d" % (chrom.name, chrom.length)
 
 main()
