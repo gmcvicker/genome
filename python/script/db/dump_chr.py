aa0457d77e85f0eaab705a7599430b4b8b4b0c85
Graham McVicker <gmcvicker@uchicago.edu>
2012-05-19 18:34:19 -0500

    when dumping chromosomes, can now specify assembly

--- python/script/db/dump_chr.py
+++ python/script/db/dump_chr.py
@@ -1,11 +1,24 @@
 import sys
 import tables
+import argparse
 
 import genome.db
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--assembly", default='hg18',
+                        help="assembly to print chromosomes for"
+                        " (e.g. hg18)")
+
+    return parser.parse_args()
+
+
 def main():
-    gdb = genome.db.GenomeDB()
+    args = parse_args()
+    
+    gdb = genome.db.GenomeDB(assembly=args.assembly)
 
     for chrom in gdb.get_chromosomes(get_rand=True,
 				     get_auto=True,
