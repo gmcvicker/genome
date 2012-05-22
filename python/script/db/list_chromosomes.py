9c9b2ab6a5c65a6ee556eedcc1ce7113d4501e95
Graham McVicker <gmcvicker@uchicago.edu>
2012-05-22 18:10:42 -0500

    added more command line options to list_chromosomes.py

--- python/script/db/list_chromosomes.py
+++ python/script/db/list_chromosomes.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 import tables
 import argparse
@@ -6,13 +8,42 @@ import genome.db
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="write chromosome names "
+                                     "and lengths to stdout")
 
     parser.add_argument("--assembly", default='hg18',
                         help="assembly to print chromosomes for"
                         " (e.g. hg18)")
 
+    parser.add_argument("--rand", action='store_true',
+                        help="include random/unknown chromosomes in output",
+                        default=False)
+
+    parser.add_argument('--y', action='store_true',
+                        help="include chrY in output",
+                        default=False)
+    
+    parser.add_argument('--hap', action='store_true',
+                        help="include alternate haplotypes in output",
+                        default=False)
+        
+    parser.add_argument('--mito', action='store_true',
+                        help='include mitochondrial chromosomes in output',
+                        default=False)
+
+    parser.add_argument('--no_x', action="store_true",
+                        default=False,
+                        help="do not include chromosome X")
+    
+
+    parser.add_argument('--all', action='store_true',
+                        help="include all chromosomes in output",
+                        default=False)
+
+    
+    
     return parser.parse_args()
+
 
 
 def main():
@@ -20,7 +51,18 @@ def main():
     
     gdb = genome.db.GenomeDB(assembly=args.assembly)
 
-    for chrom in gdb.get_all_chromosomes():
+    
+    if args.all:
+        chromosomes = gdb.get_all_chromosomes()
+    else:
+        chromosomes = gdb.get_chromosomes(get_rand=args.rand,
+                                          get_y=args.y,
+                                          get_x=(not args.no_x),
+                                          get_hap=args.hap,
+                                          get_mito=args.mito)
+
+    for chrom in chromosomes:
         print "%s\t%d" % (chrom.name, chrom.length)
+        
 
 main()
