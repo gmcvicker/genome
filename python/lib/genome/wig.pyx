98b8d52628d023f9fb6c561bf5cd8e1dabb57df0
Graham McVicker <gmcvicker@uchicago.edu>
2013-02-03 19:55:57 -0600

    allow writing of float32 to wiggle files

--- python/lib/genome/wig.pyx
+++ python/lib/genome/wig.pyx
@@ -5,9 +5,13 @@ cimport numpy as np
 
 cdef extern from "wig.h":
      void wig_write_uint8(char *filename, unsigned char *vals, char *chr_name, long chr_len)
+     void wig_write_float32(char *filename, float *vals, char *chr_name, long chr_len)
 
-ctypedef np.uint8_t DTYPE_t
+ctypedef np.uint8_t DTYPE_uint8_t
+ctypedef np.float32_t DTYPE_float32_t
 
-def write_uint8(filename, np.ndarray[DTYPE_t, ndim=1]vals, chr_name):
+def write_uint8(filename, np.ndarray[DTYPE_uint8_t, ndim=1]vals, chr_name):
      wig_write_uint8(filename, <unsigned char *>vals.data, chr_name, vals.shape[0])
 
+def write_float32(filename, np.ndarray[DTYPE_float32_t, ndim=1]vals, chr_name):
+    wig_write_float32(filename, <float *>vals.data, chr_name, vals.shape[0])
