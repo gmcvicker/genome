4f2f9473ba38abc07e371e40697754c1523caeea
Graham McVicker <gmcvicker@uchicago.edu>
2013-02-27 13:39:43 -0600

    added support for ambiguity codes

--- c/lib/nuc.c
+++ c/lib/nuc.c
@@ -6,7 +6,10 @@
 #include "util.h"
 #include "err.h"
 
-static const char NUC_SYMBOL[NUM_NUCS] = {'A', 'C', 'G', 'T', '-', 'N'};
+
+static const char NUC_SYMBOL[NUM_NUCS] = {'A', 'C', 'G', 'T', 
+					  'M', 'K', 'R', 'Y', 'W', 'S', 
+					  '-', 'N'};
 
 
 /**
@@ -20,6 +23,22 @@ char nuc_id_to_char(const unsigned char id) {
 }
 
 
+/** 
+ * returns TRUE if this is an id for an unambiguous nucleotide 
+ * (one of A, C, T, G)
+ */
+int nuc_is_real(const unsigned char id) {
+  return (id < NUC_M);
+}
+
+/*
+ * returns TRUE if this is an id for a nucleotide ambiguity code
+ * (one of M, K, R, Y, W, S).
+ */
+int nuc_is_ambi(const unsigned char id) {
+  return (id > NUC_T) && (id < NUC_GAP);
+}
+
 
 /**
  * Converts a nucleotide to a unique nucleotide integer identifier.
@@ -30,11 +49,42 @@ unsigned char nuc_char_to_id(const char nuc) {
   case('C'): case('c'): return NUC_C;
   case('T'): case('t'): return NUC_T;
   case('G'): case('g'): return NUC_G;
+  case('M'): case('m'): return NUC_M;
+  case('K'): case('k'): return NUC_K;
+  case('R'): case('r'): return NUC_R;
+  case('Y'): case('y'): return NUC_Y;
+  case('W'): case('w'): return NUC_W;
+  case('S'): case('s'): return NUC_S;
   case('.'): case('-'): case('*'): return NUC_GAP;
+  case('N'): case('n'): return NUC_N;
   }
+
+  my_warn("%s:%d: unknown nucleotide character %c",
+	  __FILE__, __LINE__, nuc);
+
   return NUC_N;
 }
 
+
+/**
+ * returns the complement of the provided nucleotide ID
+ */
+unsigned char nuc_comp(const unsigned char nuc_id) {
+  switch(nuc_id) {
+  case(NUC_A): return NUC_T;
+  case(NUC_C): return NUC_G;
+  case(NUC_G): return NUC_C;
+  case(NUC_T): return NUC_A;
+  case(NUC_M): return NUC_K;
+  case(NUC_K): return NUC_M;
+  case(NUC_R): return NUC_Y;
+  case(NUC_Y): return NUC_R;
+  case(NUC_W): return NUC_W;
+  case(NUC_S): return NUC_S;
+  }  
+  /* for N, GAP, complement is same */
+  return nuc_id;
+}
 
 
 /**
@@ -85,5 +135,21 @@ unsigned char *nuc_str_to_ids(unsigned char *buf, const char *str,
     buf[i] = nuc_char_to_id(str[i]);
   }
   return buf;
+}
+
+
+
+
+/**
+ * Does an in-place reverse complement of an array of
+ * nucleotide ids
+ */
+void nuc_ids_revcomp(unsigned char *nuc_ids, long len) {
+  long i;
+
+  util_breverse(nuc_ids, len);
+  for(i = 0; i < len; i++) {
+    nuc_ids[i] = nuc_comp(nuc_ids[i]);
+  }
 }
 
--- c/lib/nuc.h
+++ c/lib/nuc.h
@@ -5,34 +5,34 @@
 #include <ctype.h>
 #include "seq.h"
 
-enum nucleotide {NUC_A=0, NUC_C, NUC_G, NUC_T, NUC_GAP, NUC_N, NUM_NUCS};
+enum nucleotide {NUC_A=0, NUC_C, NUC_G, NUC_T, 
+		 NUC_M, /* [M]ethyl: A or C */
+		 NUC_K, /* [K]eto: G or T */
+		 NUC_R, /* pu[R]ine: A or G */
+		 NUC_Y, /* p[Y]rimidine: C or T */
+		 NUC_W, /* [W]eak: A or T */
+		 NUC_S, /* [S]trong: G or C */
+		 NUC_GAP, NUC_N, NUM_NUCS};
 
 /* the number of "true" nucleotides, i.e. not gaps or ambiguity chars */
 #define NUM_REAL_NUCS 4
 
-
-/* macro function, complements a nucleotide ID */
-#define nuc_comp(x) ((x==NUC_A) ? NUC_T : \
-                    ((x==NUC_T) ? NUC_A : \
-                    ((x==NUC_G) ? NUC_C : \
-                    ((x==NUC_C) ? NUC_G : x))))
-
 char nuc_id_to_char(const unsigned char id);
+
 unsigned char nuc_char_to_id(const char nuc);
 
+int nuc_is_real(const unsigned char id);
+
+int nuc_is_ambi(const unsigned char id);
+
+unsigned char nuc_comp(const unsigned char nuc_id);
+
 char *nuc_ids_to_str(char *buf, const unsigned char *ids, const long len);
+
 unsigned char *nuc_str_to_ids(unsigned char *buf, const char *str, 
 			      const long len);
 
-double **nuc_read_matrix(const char *filename);
-
-void nuc_write_matrix_dbl(FILE *fh, double **matrix);
-void nuc_write_matrix_ul(FILE *fh, long **matrix);
-
-long **nuc_matrix_ul_new(void);
-double **nuc_matrix_dbl_new(void);
-
-void nuc_matrix_ul_set_zero(long **matrix);
+void nuc_ids_revcomp(unsigned char *nuc_ids, const long len);
 
 
 #endif
