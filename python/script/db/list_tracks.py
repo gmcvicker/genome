f977d52c487f807b9547f035df99c1ebab7e741b
Graham McVicker <gmcvicker@uchicago.edu>
2013-12-11 17:25:05 -0600

    added --path option to print full list of tracks instead of track names

--- python/script/db/list_tracks.py
+++ python/script/db/list_tracks.py
@@ -5,10 +5,16 @@ import genome.db
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--assembly", metavar="ASSEMBLY", default="hg18",
-                    help="Assembly to list tracks for (e.g. hg18)")
-args = parser.parse_args()
 
+parser.add_argument("--assembly", metavar="ASSEMBLY", default=None,
+                    help="Assembly to list tracks for (e.g. hg18)")
+
+parser.add_argument("--paths", dest="paths", action="store_true",
+                    default=False, help="print full paths to "
+                    ".h5 files, rather than tracknames")
+
+                    
+args = parser.parse_args()
 
 gdb = genome.db.GenomeDB(assembly=args.assembly)
 
@@ -16,4 +22,7 @@ tracknames = gdb.list_tracks()
 tracknames.sort()
 
 for trackname in tracknames:
-    print trackname
+    if args.paths:
+        print gdb.get_track_path(trackname)
+    else:
+        print trackname
