3454dba34fe877b579e91a721712a73a45fb6529
Graham McVicker <gmcvicker@gmail.com>
2015-12-23 15:50:15 -0600

    write results in matrix format

--- c/program/smith_waterman.c
+++ c/program/smith_waterman.c
@@ -10,20 +10,23 @@
 
 
 
+
+
 int main(int argc, char **argv) {
   gzFile q_fastq_gz, t_fasta_gz;
   FastqSeq read;
-  Seq *t_seq, *q_seq;
+  Seq *t_seq, *q_seq, **t_seq_array;
   int status;
   AlnNode **matrix, *end;
   int **score_matrix;
   long max_reads;
+  long n_t_seq, i, read_num;
   long n_row, n_col;
   long n_reads, total_score, n_pos_score, n_neg_score;
-  double mean_score;
 
   if(argc < 3) {
-    fprintf(stderr, "usage: %s query_seqs1.fq target_seqs2.fa [max_reads]\n", argv[0]);
+    fprintf(stderr, "usage: %s query_seqs1.fq target_seqs2.fa [max_reads]\n",
+	    argv[0]);
     exit(2);
   }
 
@@ -31,10 +34,10 @@ int main(int argc, char **argv) {
 				      ALN_DEFAULT_MISMATCH_SCORE,
 				      ALN_DEFAULT_OTHER_SCORE);
 
-  t_seq = seq_new();
   q_seq = seq_new();
 
   t_fasta_gz = util_must_gzopen(argv[2], "rb");
+  t_seq_array = seq_read_fasta_all(t_fasta_gz, &n_t_seq);
 
   if(argc > 3) {
     max_reads = util_parse_long(argv[3]);
@@ -49,35 +52,43 @@ int main(int argc, char **argv) {
 
 
   /** TODO: change to write matrix of alignments **/
+  q_fastq_gz = util_must_gzopen(argv[1], "rb");
 
-  while((seq_read_fasta_record(t_seq, t_fasta_gz)) >= 0) {
+  n_reads = 0;
+  total_score = 0;
+  n_pos_score = 0;
+  n_neg_score = 0;
 
-    fprintf(stderr, "--------\n%s\n", t_seq->name);
-    q_fastq_gz = util_must_gzopen(argv[1], "rb");
+  /* write header */
+  fprintf(stdout, "READ.NUM\tREAD.LEN");
+  for(i = 0; i < n_t_seq; i++) {
+    fprintf(stdout, "\t%s", t_seq_array[i]->name);
+  }
+  fprintf(stdout, "\n");
 
-    /* expand alignment matrix if needed */
-    if(n_col < t_seq->len) {
-      n_col = t_seq->len * 2;
-      fprintf(stderr, "expanding alignment matrix to %ldx%ld\n", n_row, n_col);
-      aln_matrix_free(matrix);
-      matrix = aln_matrix_new(n_row, n_col);
+  read_num = 0;
+  while((status = fastq_parse_read(&read, q_fastq_gz)) != FASTQ_END) {
+    if(status == FASTQ_ERR) {
+      my_warn("%s:%d: error parsing read", __FILE__, __LINE__);
     }
 
-    n_reads = 0;
-    total_score = 0;
-    n_pos_score = 0;
-    n_neg_score = 0;
+    seq_read_seqstr(q_seq, read.line2);
+    read_num += 1;
 
-    while((status = fastq_parse_read(&read, q_fastq_gz)) != FASTQ_END) {
-      if(status == FASTQ_ERR) {
-	my_warn("%s:%d: error parsing read", __FILE__, __LINE__);
-      }
+    /* TODO: could add seq name etc */
+    fprintf(stdout, "%ld\t%ld", read_num, q_seq->len);
 
-      seq_read_seqstr(q_seq, read.line2);
+    for(i = 0; i < n_t_seq; i++) {
+      t_seq = t_seq_array[i];
 
       /* expand alignment matrix if needed */
-      if(n_row < q_seq->len) {
-	n_row = q_seq->len * 2;
+      if(n_row < q_seq->len || n_col < t_seq->len) {
+	if(n_row < q_seq->len) {
+	  n_row = q_seq->len * 2;
+	}
+	if(n_col < t_seq->len) {
+	  n_col = t_seq->len * 2;
+	}
 	fprintf(stderr, "expanding alignment matrix to %ldx%ld\n",
 		n_row, n_col);
 	aln_matrix_free(matrix);
@@ -87,41 +98,36 @@ int main(int argc, char **argv) {
       end = aln_local(matrix, score_matrix, ALN_DEFAULT_GAP_OPEN_SCORE,
 		      ALN_DEFAULT_GAP_EXT_SCORE, q_seq, t_seq);
 
-      fprintf(stdout, "%s\t%s\t%ld\t%ld\n", t_seq->name, q_seq->name,
-	      q_seq->len, end->score);
+      fprintf(stdout, "\t%ld", end->score);
 
+      /* aln_write(stdout, end, q_seq, t_seq); */
+    }
+    fprintf(stdout, "\n");
 
-      aln_write(stdout, end, q_seq, t_seq);
-
-
-      n_reads += 1;
-      total_score += end->score;
-      if(end->score >= 0) {
-	n_pos_score += 1;
-      } else {
-	n_neg_score += 1;
-      }
-
-      if(max_reads > 0 && n_reads >= max_reads) {
-	fprintf(stderr, "stopping because reached max reads: %ld\n", max_reads);
-	break;
-      }
+    n_reads += 1;
+    total_score += end->score;
+    if(end->score >= 0) {
+      n_pos_score += 1;
+    } else {
+      n_neg_score += 1;
     }
 
-    gzclose(q_fastq_gz);
-
-    /* mean_score = (double)total_score / (double)n_reads; */
-
-    /* fprintf(stdout, "%s\t%ld\t%ld\t%ld\t%ld\t%.2f\n", */
-    /* 	    t_seq->name, n_reads, total_score, n_pos_score, n_neg_score, */
-    /* 	    mean_score); */
+    if(max_reads > 0 && n_reads >= max_reads) {
+      fprintf(stderr, "stopping because reached max reads: %ld\n", max_reads);
+      break;
+    }
   }
+
+  gzclose(q_fastq_gz);
 
 
   fprintf(stderr, "freeing memory\n");
   aln_matrix_free(matrix);
   aln_score_matrix_free(score_matrix);
-  seq_free(t_seq);
+  for(i = 0; i < n_t_seq; i++) {
+    seq_free(t_seq_array[i]);
+  }
+  my_free(t_seq_array);
   seq_free(q_seq);
   gzclose(t_fasta_gz);
 }
